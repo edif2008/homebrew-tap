a2051ea288ec6ea8efc313fd25ae8856f18a5cfb
Eduard Filip <edif201213@gmail.com>
2022-12-15 14:21:07 +0200

    Create my-app.rb

---
+++ Formula/my-app.rb
@@ -0,0 +1,20 @@
+class Foo < Formula
+  desc ""
+  homepage ""
+  url "https://example.com/foo-0.1.tar.gz"
+  sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
+  license ""
+
+  # depends_on "cmake" => :build
+
+  def install
+    # ENV.deparallelize
+    system "./configure", *std_configure_args, "--disable-silent-rules"
+    # system "cmake", "-S", ".", "-B", "build", *std_cmake_args
+    system "make", "install"
+  end
+
+  test do
+    system "false"
+  end
+end
