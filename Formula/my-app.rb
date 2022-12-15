b79cc779cd0e6f32c9ae48b28c2b6194bcfa95f6
Eduard Filip <edif201213@gmail.com>
2022-12-15 14:23:55 +0200

    Update my-app.rb

--- Formula/my-app.rb
+++ Formula/my-app.rb
@@ -1,8 +1,8 @@
 class MyApp < Formula
   desc ""
   homepage ""
-  url "https://example.com/foo-0.1.tar.gz"
-  sha256 "85cc828a96735bdafcf29eb6291ca91bac846579bcef7308536e0c875d6c81d7"
+  url "https://github.com/DOCGroup/ACE_TAO/releases/download/ACE%2BTAO-7_0_10/ACE+TAO-7.0.10.tar.bz2"
+  sha256 "cba005e2d267884ba9634414bd39c2a72b8717eb9f7a7a293298f3b2a05c841e"
   license ""
 
   # depends_on "cmake" => :build
